d6aaee601407d0385b319c03f234b0fd8a8adaf5
ResulticksInc <mobile.sdk@resulticks.com>
2019-09-05 11:56:45 +0530

    Update ReCordovaPlugin.m

--- src/ios/ReCordovaPlugin.m
+++ src/ios/ReCordovaPlugin.m
@@ -1,35 +1,28 @@
-/********* ResulticksPlugin.m Cordova Plugin Implementation *******/
+/********* ReCordovaPlugin.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
 
 @interface ReCordovaPlugin : CDVPlugin {
   // Member variables go here.
 }
-- (void)userRegister:(CDVInvokedUrlCommand*)command;
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation ReCordovaPlugin
-- (void)userRegister:(CDVInvokedUrlCommand*)command
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"userRegister %@",echo);
-        NSString uniqueId = [[command.arguments objectAtIndex:0] valueForKey:@"uniqueId"];
-        NSString name = [[command.arguments objectAtIndex:0] valueForKey:@"name"];
-        NSString email = [[command.arguments objectAtIndex:0] valueForKey:@"email"];
-        NSString phone = [[command.arguments objectAtIndex:0] valueForKey:@"phone"];
-        NSString age = [[command.arguments objectAtIndex:0] valueForKey:@"age"];
-        NSString gender = [[command.arguments objectAtIndex:0] valueForKey:@"gender"];
-        NSString token = [[command.arguments objectAtIndex:0] valueForKey:@"token"];
-        NSString profileUrl = [[command.arguments objectAtIndex:0] valueForKey:@"profileUrl"];
 
+    if (echo != nil && [echo length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 
-
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
