259de389234d4c983c5d2422a9d99b2ae72c7974
ResulticksInc <mobile.sdk@resulticks.com>
2019-09-05 11:54:36 +0530

    Update ReCordovaPlugin.m

--- src/ios/ReCordovaPlugin.m
+++ src/ios/ReCordovaPlugin.m
@@ -6,13 +6,9 @@
   // Member variables go here.
 }
 - (void)userRegister:(CDVInvokedUrlCommand*)command;
-- (void)customEvent:(CDVInvokedUrlCommand*)command;
-- (void)screenNavigation:(CDVInvokedUrlCommand*)command;
-- (void)locationUpdate:(CDVInvokedUrlCommand*)command;
 @end
 
 @implementation ReCordovaPlugin
-
 - (void)userRegister:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
@@ -36,56 +32,5 @@
 
 
 }
-
-- (void)customEvent:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"customEvent %@",echo);
-
-         NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"eventName"];
-         NSString data = [[command.arguments objectAtIndex:0] valueForKey:@"data"];
-
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-}
-
-- (void)screenNavigation:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"screenNavigation %@",echo);
-
-        NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"screenName"];
-
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-}
-
-- (void)locationUpdate:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"locationUpdate %@",echo);
-         NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"latitude"];
-         NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"longitude"];
-
-
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-}
-
 
 @end
