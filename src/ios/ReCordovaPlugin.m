dbc0d56f83806bb4e789202bfcf8889e823fd232
ResulticksInc <mobile.sdk@resulticks.com>
2019-09-26 17:24:50 +0530

    ios updated

--- src/ios/ReCordovaPlugin.m
+++ src/ios/ReCordovaPlugin.m
@@ -1,90 +1,166 @@
 /********* ResulticksPlugin.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <REIOSSDK/REIOSSDK.h>
 
 @interface ReCordovaPlugin : CDVPlugin {
-  // Member variables go here.
+    // Member variables go here.
 }
 - (void)userRegister:(CDVInvokedUrlCommand*)command;
 - (void)customEvent:(CDVInvokedUrlCommand*)command;
 - (void)screenNavigation:(CDVInvokedUrlCommand*)command;
 - (void)locationUpdate:(CDVInvokedUrlCommand*)command;
+- (void)getNotification:(CDVInvokedUrlCommand*)command;
+- (void)deleteNotification:(CDVInvokedUrlCommand*)command;
+//- (void)onNotificationPayloadReceiver:(CDVInvokedUrlCommand*)command;
+
 @end
 
 @implementation ReCordovaPlugin
 
-- (void)userRegister:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"userRegister %@",echo);
+- (void)userRegister:(CDVInvokedUrlCommand*)command {
 
-        NSString uniqueId = [[command.arguments objectAtIndex:0] valueForKey:@"uniqueId"];
-        NSString name = [[command.arguments objectAtIndex:0] valueForKey:@"name"];
-        NSString email = [[command.arguments objectAtIndex:0] valueForKey:@"email"];
-        NSString phone = [[command.arguments objectAtIndex:0] valueForKey:@"phone"];
-        NSString age = [[command.arguments objectAtIndex:0] valueForKey:@"age"];
-        NSString gender = [[command.arguments objectAtIndex:0] valueForKey:@"gender"];
-        NSString token = [[command.arguments objectAtIndex:0] valueForKey:@"token"];
-        NSString profileUrl = [[command.arguments objectAtIndex:0] valueForKey:@"profileUrl"];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
+    CDVPluginResult *pluginResult = nil;
+    NSDictionary *params = [command.arguments firstObject];
+
+    NSLog(@"userRegister token %@",params);
 
 
-}
+    if (params != nil && [params count] > 0) {
 
-- (void)customEvent:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"customEvent %@",echo);
+        NSLog(@"userRegister token %@",params);
 
-         NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"eventName"];
-         NSString data = [[command.arguments objectAtIndex:0] valueForKey:@"data"];
+        [REiosHandler sdkRegistrationWithDictWithParams:params];
 
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"params"];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 }
 
-- (void)screenNavigation:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"screenNavigation %@",echo);
+- (void)customEvent:(CDVInvokedUrlCommand*)command {
 
-        NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"screenName"];
+    CDVPluginResult *pluginResult = nil;
+    NSDictionary *params = [command.arguments firstObject];
 
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    if (params != nil && [params count] > 0) {
+
+        NSString *eventName = [params valueForKey:@"eventName"];
+        NSDictionary *dataDict = [params valueForKey:@"data"];
+        NSLog(@"customEvent %@",dataDict);
+
+
+        NSError * err;
+        NSData * jsonData = [NSJSONSerialization  dataWithJSONObject:dataDict options:0 error:&err];
+        NSString * dataString = [[NSString alloc] initWithData:jsonData   encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",dataString);
+
+        [REiosHandler addEventWithEventName:eventName data:dataString];
+
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"params"];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 }
 
-- (void)locationUpdate:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-    
-    if (echo != nil && [echo length] > 0) {
-        NSLog(@"locationUpdate %@",echo);
-         NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"latitude"];
-         NSString eventName = [[command.arguments objectAtIndex:0] valueForKey:@"longitude"];
+- (void)screenNavigation:(CDVInvokedUrlCommand*)command {
 
+    CDVPluginResult *pluginResult = nil;
+    NSDictionary *params = [command.arguments firstObject];
 
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    if (params != nil && [params count] > 0) {
+
+        NSString *screenName = [params valueForKey:@"screenName"];
+        NSLog(@"screenName %@",screenName);
+
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"params"];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
 }
+
+- (void)locationUpdate:(CDVInvokedUrlCommand*)command {
+
+    CDVPluginResult *pluginResult = nil;
+    NSDictionary *params = [command.arguments firstObject];
+
+    if (params != nil && [params count] > 0) {
+
+        NSNumber *latitude = [params valueForKey:@"latitude"];
+        NSNumber *longitude = [params valueForKey:@"longitude"];
+        NSLog(@"latitude %@",latitude);
+        NSLog(@"longitude %@",longitude);
+
+        [REiosHandler updateLocationWithLat:[latitude stringValue] long:[longitude stringValue]];
+
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"params"];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+}
+
+- (void)getNotification:(CDVInvokedUrlCommand*)command {
+
+    NSLog(@"command.argument %@", command.arguments);
+
+    NSError* error = nil;
+
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:[REiosHandler getNotificationList] options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+
+    NSLog(@"Notification list json string %@", jsonString);
+
+    CDVPluginResult* pluginResult = nil;
+
+    @try {
+
+        if (jsonString != nil && [jsonString length] > 0) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:jsonString];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
+    } @catch (NSException* exception) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_JSON_EXCEPTION messageAsString:[exception reason]];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+}
+
+- (void)deleteNotification:(CDVInvokedUrlCommand*)command {
+
+    CDVPluginResult *pluginResult = nil;
+    NSDictionary<NSString *, id> *params = [command.arguments firstObject];
+
+    if (params != nil && [params count] > 0) {
+
+        NSLog(@"deleteNotification %@",params);
+
+        [REiosHandler deleteNotificationListWithDict:params];
+
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"params"];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+}
+
+//- (void)onNotificationPayloadReceiver:(CDVInvokedUrlCommand*)command {
+//
+//    CDVPluginResult *pluginResult = nil;
+//    NSDictionary *params = [command.arguments firstObject];
+//
+//    if (params != nil && [params count] > 0) {
+//
+//        NSLog(@"longitude %@",params);
+//
+//        [[REiosHandler getNotification] setNotificationActionWithResponse:@{}];
+//
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"params"];
+//    } else {
+//        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//    }
+//}
+
+
 
 
 @end
