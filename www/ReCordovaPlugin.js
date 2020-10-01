48f8e9e2548b1c4b2e32630fab7d15517e40f9e2
ResulticksInc <36412134+ResulticksInc@users.noreply.github.com>
2020-10-01 11:44:25 +0530

    iOS Added FormData Capture API

--- www/ReCordovaPlugin.js
+++ www/ReCordovaPlugin.js
@@ -67,9 +67,10 @@ module.exports.unReadNotification = function (arg0, success, error) {
 	exec(success, error, 'ReCordovaPlugin', 'unReadNotification', [arg0]);
 };
 
-module.exports.notificationCTAClicked = function(arg0,success, error) {
-	exec(success, error, 'ReCordovaPlugin', 'notificationCTAClicked', [ arg0 ]);
+module.exports.notificationCTAClicked = function (arg0, success, error) {
+	exec(success, error, 'ReCordovaPlugin', 'notificationCTAClicked', [arg0]);
 };
+
 module.exports.handleQrLink = function(arg0,success, error) {
 	exec(success, error, 'ReCordovaPlugin', 'handleQrLink', [ arg0 ]);
 };
@@ -81,8 +82,9 @@ module.exports.getDeepLinkData = function(arg0,success, error) {
 module.exports.appConversionTracking = function(arg0,success,error) {
 	exec(success, error, 'ReCordovaPlugin', 'appConversionTracking', [ arg0 ]);
 }
-
-
+module.exports.formDataCapture = function(arg0,success,error) {
+	exec(success, error, 'ReCordovaPlugin', 'formDataCapture', [ arg0 ]);
+}
 
 module.exports.getViewJson = function (arg0, success, error) {
 	getViewJsons();
